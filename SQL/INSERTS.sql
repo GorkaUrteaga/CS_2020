8d6e4ef587a2c0adff391d8d4fb8ded6ee557aa9
unknown <gorka32x@gmail.com>
2020-05-27 17:04:41 +0200

    Frontend registro preparado

--- SQL/INSERTS.sql
+++ SQL/INSERTS.sql
@@ -3,8 +3,8 @@
 */
 
 /* INSERT ADMINISTRADORS Marc i Gorka */
-INSERT INTO USUARIO (email, password, es_admin, activado) VALUES ('03acdad1148f03a359b52d96e5255932','dc6ae7fd83dab1a4a706311971b6f2aa', true, true);
-INSERT INTO USUARIO (email, password, es_admin, activado) VALUES ('d09663e78d67c9e8291134cde7ab014a','983a21a1216c2f1e25479180031f58bb', true, true);
+INSERT INTO USUARIO (email, password, es_admin, activado) VALUES ('03acdad1148f03a359b52d96e5255932','983a21a1216c2f1e25479180031f58bb', true, true);
+INSERT INTO USUARIO (email, password, es_admin, activado) VALUES ('d09663e78d67c9e8291134cde7ab014a','dc6ae7fd83dab1a4a706311971b6f2aa', true, true);
 
 /* Sempre han de donar 100% */ 
 INSERT INTO SINTOMA (nombre, porcentaje) VALUES ('Fiebre',40);
